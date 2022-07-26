85a3ba2323ce9acea0e6d1004a7a89a72bcbde03
Archflayd <krzysztofxkwiecien@gmail.com>
2022-07-26 19:22:32 +0200

    Initial commit

---
+++ src/main/java/com/kk/strong/StrongApplication.java
@@ -0,0 +1,12 @@
+package com.kk.strong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrongApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StrongApplication.class, args);
+    }
+
+}
