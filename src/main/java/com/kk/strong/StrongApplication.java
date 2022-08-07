51d4999a2b158b4e63d25f681841faff9792cc24
Archflayd <krzysztofxkwiecien@gmail.com>
2022-08-07 23:32:03 +0200

    Security config for authentication and authorization

--- src/main/java/com/kk/strong/StrongApplication.java
+++ src/main/java/com/kk/strong/StrongApplication.java
@@ -1,12 +1,35 @@
 package com.kk.strong;
 
+import com.kk.strong.model.User;
+import com.kk.strong.model.UserRole;
+import com.kk.strong.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
 
 @SpringBootApplication
 public class StrongApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(StrongApplication.class, args);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(UserService userService) {
+        return args ->
+        {
+            userService.saveUser(new User(1L, "Adam", "adam", "adam", List.of(UserRole.REGULAR_USER)));
+            userService.saveUser(new User(2L, "Eve", "eve", "eve", List.of(UserRole.REGULAR_USER, UserRole.PREMIUM_USER)));
+        };
+    }
 }
