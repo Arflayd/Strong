8542ca38f0ff67b39a5cb4ad36d33c23d84b404f
Archflayd <krzysztofxkwiecien@gmail.com>
2022-08-09 23:30:30 +0200

    Added model entities

--- src/main/java/com/kk/strong/model/User.java
+++ src/main/java/com/kk/strong/model/User.java
@@ -18,9 +18,18 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String name;
     private String username;
     private String password;
+
+    private String name;
+    private GenderType gender;
+    private int age;
+    private int height;
+
+    @OneToMany
+    private List<BodyReport> bodyReports = new ArrayList<>();
+    @OneToMany
+    private List<WorkoutSession> workoutSessions = new ArrayList<>();
 
     @ElementCollection
     @Enumerated(value = EnumType.STRING)
