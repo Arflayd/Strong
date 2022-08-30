ec20a6f942dc81bf6a39ada207fbc91c93ea5e51
Archflayd <krzysztofxkwiecien@gmail.com>
2022-08-31 00:37:01 +0200

    DTOs for the model

---
+++ src/main/java/com/kk/strong/model/dto/UserDto.java
@@ -0,0 +1,19 @@
+package com.kk.strong.model.dto;
+
+import com.kk.strong.model.GenderType;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class UserDto {
+
+    private Long id;
+    private String name;
+    private GenderType gender;
+    private int age;
+    private int height;
+    private List<BodyReportDto> bodyReports = new ArrayList<>();
+    private List<WorkoutSessionDto> workoutSessions = new ArrayList<>();
+}
